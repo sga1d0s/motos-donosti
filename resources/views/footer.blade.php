bf6fcabb4e239d3f58c6878aa2ef746c71c06ed4
Sergio Galdos <galdos.sergio@outlook.com>
2025-07-05 04:35:18 +0200

    [dev] ADD buttons on footer

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,4 +1,4 @@
 <footer class="bg-gray-100 p-4 text-center">
-  <a href="{{ route('pantalla1') }}" class="btn btn-primary mx-2">Pantalla 1</a>
-  <a href="{{ route('pantalla2') }}" class="btn btn-secondary mx-2">Pantalla 2</a>
+  <button class="primary" href="{{ route('motos.index') }}" class="btn btn-primary mx-2">Listado</button>
+  <button class="primary" href="{{ route('motos.index') }}" class="btn btn-secondary mx-2">Reservas</button>
 </footer>
