cf40753d7cadc98b0f3a526df083fbc587fb0f35
Sergio Galdos <galdos.sergio@outlook.com>
2025-07-05 03:59:36 +0200

    [dev] ADD footer

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,4 @@
+<footer class="bg-gray-100 p-4 text-center">
+  <a href="{{ route('pantalla1') }}" class="btn btn-primary mx-2">Pantalla 1</a>
+  <a href="{{ route('pantalla2') }}" class="btn btn-secondary mx-2">Pantalla 2</a>
+</footer>
